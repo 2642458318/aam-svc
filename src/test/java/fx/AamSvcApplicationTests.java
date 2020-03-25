bf8fdf437a76495fd2ed207e5ca390c0fa88a1fe
fuxiao <2642458318@qq.com>
2020-03-25 22:43:39 +0800

    创建项目

---
+++ src/test/java/fx/AamSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package fx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AamSvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
